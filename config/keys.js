062a1036091fcaf51db87651ec69f1ef673d56ad
Priyank Gajera <priyankgajera67@hotmail.com>
2019-10-11 19:26:09 -0400

    Commti for angular.

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://dailytask0-teimo.mongodb.net/test",
+  mongoURI: "mongodb+srv://Priyank:Mighty@6768@dailytask0-teimo.mongodb.net/test",
   secretOrKey: "secret"
 };
