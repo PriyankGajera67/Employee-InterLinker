ee7e632a72a822f3637b7f71218e4130d1e0e9e4
Priyank Gajera <priyankgajera67@hotmail.com>
2019-10-11 18:14:40 -0400

    Commit for heroku changes.

---
+++ src/app/_services/company.service.ts
@@ -0,0 +1,22 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+
+import { environment } from '../../environments/environment';
+import { User } from '../_models';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+    constructor(private http: HttpClient) { }
+
+    getAll() {
+        return this.http.get<User[]>(`${environment.apiUrl}/users`);
+    }
+
+   
+    registerUser(name: string,email: string, password: string, confirmPassword: string) {
+        let role = 'COMPANY';
+        return this.http.post<any>(`${environment.apiUrl}/users/register`, { name,email,password,confirmPassword,role });
+    }
+}
