8275c0ed4a4a55c45fe4ad0274c11f29b6a733d4
Priyank Gajera <priyankgajera67@hotmail.com>
2019-10-11 20:59:23 -0400

    Commit for ssl changes.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://employee-interlinker.herokuapp.com/api'
+    apiUrl: 'https://employee-interlinker.herokuapp.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'http://employee-interlinker.herokuapp.com/api'
+    apiUrl: 'https://employee-interlinker.herokuapp.com/api'
 };
 
 /*
