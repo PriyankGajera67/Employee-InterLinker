a344c14c7db10ee7d8dfb271732187a23831df97
Priyank Gajera <priyankgajera67@hotmail.com>
2019-10-11 20:53:53 -0400

    Commit for ssl changes.

--- server.js
+++ server.js
@@ -9,6 +9,24 @@ var cors = require('cors');
 const path = require('path');
 
 const app = express();
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
 app.use(cors());
 
 // Bodyparser middleware
