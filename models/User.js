34a6fa7e78231a836c17e0f994ce02e7aef59e15
Priyank Gajera <priyankgajera67@hotmail.com>
2019-10-15 15:58:10 -0400

    Commit for changes in user collection.

--- models/User.js
+++ models/User.js
@@ -22,6 +22,67 @@ const UserSchema = new Schema({
   role:{
     type:String,
     default:"REGULAR"
+  },
+  employer:{
+    type:String,
+    required:false
+  },
+  fullTime:{
+    type:Boolean,
+    default:false
+  },
+  partTime:{
+    type:Boolean,
+    default:false
+  },
+  webSite:{
+    type:String,
+    default:"www.xyz.com"
+  },
+  gender:{
+    type:String,
+    default:'Male'
+  },
+  contactNumber:{
+    type:String,
+    Default:'9999999999',
+    maxlength:10
+  },
+  verified:{
+    type:Boolean,
+    default:false
+  },
+  avatar:{
+    type:Boolean,
+    default:'https://img.pngio.com/avatar-user-computer-icons-software-developer-avatar-png-png-computer-user-900_540.jpg'
+  },
+  dob:{
+    type:String,
+    required:false
+  },
+  numbersOfEmployees:{
+    type:Number,
+    default:0
+  },
+  joiningDate:{
+    type:String,
+    required:false
+  },
+  address:{
+    type:String,
+    required:false
+  },
+  postalCode:{
+    type:String,
+    required: false
+  },
+  city:{
+    type:String,
+    required:false
+  },
+  country:{
+    type:String,
+    required:false
   }
 });
 
