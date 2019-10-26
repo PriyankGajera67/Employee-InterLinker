152dcdb8ad2d74ac959c02890d47d10febdb88ea
Priyank Gajera <priyankgajera67@hotmail.com>
2019-10-26 18:43:04 -0400

    Commit for user-profile and home page template.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import { LogInComponent } from './components/log-in/log-in.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { UserProfileComponent } from './components/user/user-profile/user-profile.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LogInComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: UserProfileComponent, canActivate: [AuthGuard]},
   { path: 'admin', component: AdminComponent, canActivate: [AuthGuard]},
 
   // otherwise redirect to home
