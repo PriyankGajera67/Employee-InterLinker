93850b513f945cb58eadf1be6dfe83ac363ecdd2
Priyank Gajera <priyankgajera67@hotmail.com>
2019-11-16 17:26:41 -0500

    Commit for Company Profile changes.

--- routes/api/users.js
+++ routes/api/users.js
@@ -153,23 +153,10 @@ router.post("/delete", (req, res) => {
 
 router.post("/updateUser", (req, res) => {
   data = req.body.profileData;
+  
   User.findOne({ email: data.email }).then(user => {
       if (user) {
-        User.updateOne({ email: req.body.profileData.email }, {
-          "email": req.body.profileData.email,
-          "dob": req.body.profileData.dob,
-          "name": req.body.profileData.name,
-          "address":req.body.profileData.address,
-          "postalCode": req.body.profileData.postalCode,
-          "country": req.body.profileData.country,
-          "city": req.body.profileData.city,
-          "joiningDate": req.body.profileData.joiningDate,
-          "gender": req.body.profileData.gender,
-          "employer": req.body.profileData.employer,
-          "contactNumber": req.body.profileData.contactNumber,
-          "fullTime": req.body.profileData.fullTime,
-          "position":req.body.profileData.position
-        }, (err) => {
+        User.updateOne({ email: req.body.profileData.email }, data, (err) => {
           if (err) return res.json({ success: false, error: err });
           return res.json({ success: true });
         });
