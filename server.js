b18d094ee6ee6b9e297caaab15305442d19e6037
Priyank Gajera <priyankgajera67@hotmail.com>
2019-11-16 16:20:02 -0500

    Commit for roll back local config.

--- server.js
+++ server.js
@@ -14,20 +14,20 @@ const app = express();
 // a protocol other than HTTPS,
 // redirect that request to the
 // same url but with HTTPS
-// const forceSSL = function() {
-//   return function (req, res, next) {
-//     if (req.headers['x-forwarded-proto'] !== 'https') {
-//       return res.redirect(
-//        ['https://', req.get('Host'), req.url].join('')
-//       );
-//     }
-//     next();
-//   }
-// }
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
 // Instruct the app
 // to use the forceSSL
 // middleware
-//app.use(forceSSL());
+app.use(forceSSL());
 app.use(cors());
 
 // Bodyparser middleware
@@ -41,13 +41,13 @@ app.use(bodyParser.json());
 
 // Create link to Angular build directory
 
-// var distDir = __dirname + "/dist/";
+var distDir = __dirname + "/dist/";
 
-// app.use(express.static(distDir));
+app.use(express.static(distDir));
 
-// app.get('/*', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/dist/index.html'));
-// });
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 // DB Config
 const db = require("./config/keys").mongoURI;
