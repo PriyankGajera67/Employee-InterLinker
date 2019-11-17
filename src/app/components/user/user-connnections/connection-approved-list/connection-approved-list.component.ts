af1d85e838ff7d6f1a9c27769ede13f777d2ccd9
Priyank Gajera <priyankgajera67@hotmail.com>
2019-11-17 18:35:33 -0500

    Commit for UI changes/

--- src/app/components/user/user-connnections/connection-approved-list/connection-approved-list.component.ts
+++ src/app/components/user/user-connnections/connection-approved-list/connection-approved-list.component.ts
@@ -1,5 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ConnectionsService } from 'src/app/_services';
+import { MatTableDataSource, MatPaginator } from '@angular/material';
+
+export interface ConnectionElement {
+  avatar: string;
+  name: string;
+  position: string;
+  view:string;
+  action: string;
+  bio:string;
+}
 
 @Component({
   selector: 'app-connection-approved-list',
@@ -11,19 +21,39 @@ export class ConnectionApprovedListComponent implements OnInit {
   constructor(private connectionsService:ConnectionsService) { }
   myConnections:any;
   currentUserId:any;
+  employeeList:ConnectionElement[] = [];
+
+  displayedColumns: string[] = ["avatar", "name", "position","view","action"];
+  dataSource:any;
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
   ngOnInit() {
     this.currentUserId = JSON.parse(localStorage.getItem('currentUser')).id;
     this.connectionsService.getMyConnectionRequestsApproved(this.currentUserId).subscribe(res =>{
       this.setConnections(res);
     })
   }
+  
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
   setConnections(res){
-    let connections = [];
+    this.employeeList = [];
     res.data.forEach(element => {
-      connections.push(element);
+      this.employeeList.push({
+        avatar: element.avatar,
+        name: element.name,
+        position: element.position,
+        view:element.userId,
+        action: element._id,
+        bio:element.bio,
+      });
     });
-    this.myConnections = connections;
+    this.dataSource = new MatTableDataSource (this.employeeList);
+    this.dataSource.paginator = this.paginator;
+    console.log("fasdfasdfa",this.dataSource);
   }
 
   removeConnection(id){
